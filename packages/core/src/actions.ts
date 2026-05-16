187b83de82173c8dc8b882d1474c04c9141a3a22
MoAlHemyari <moalhemyari0@gmail.com>
2026-05-16 14:39:24 +0300

    rename actions names and functions to be mark-oriented

--- packages/core/src/actions.ts
+++ packages/core/src/actions.ts
@@ -17,24 +17,24 @@ const actionKinds = [
   "quest_create",
   "quest_update_title",
   "quest_update_description",
-  "quest_pause",
-  "quest_idle",
-  "quest_abandon",
-  "quest_complete",
-  "quest_remove",
-  "quest_restore",
+  "quest_mark_paused",
+  "quest_mark_idle",
+  "quest_mark_abandoned",
+  "quest_mark_completed",
+  "quest_mark_removed",
+  "quest_mark_active",
 
   // note actions
   "note_create",
   "note_update",
-  "note_remove",
-  "note_restore",
+  "note_mark_removed",
+  "note_mark_active",
 
   // progress actions
   "progress_create",
   "progress_update",
-  "progress_remove",
-  "progress_restore"
+  "progress_mark_removed",
+  "progress_mark_active"
 ] as const
 type ActionKind = (typeof actionKinds)[number]
 
@@ -131,9 +131,13 @@ export function updateQuestDescription(
   return { ok: true, action: "quest_update_description", value: updatedQuest }
 }
 
-export function pauseQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
+export function markQuestPaused(quest: Quest): ActionResult<Quest, ErrorCode> {
   if (quest.pausedAt)
-    return { ok: false, action: "quest_pause", error: allErrorCodes["QUEST_STATUS_ALREADY_PAUSED"] }
+    return {
+      ok: false,
+      action: "quest_mark_paused",
+      error: allErrorCodes["QUEST_STATUS_ALREADY_PAUSED"]
+    }
 
   const updatedQuest: Quest = {
     ...quest,
@@ -148,12 +152,16 @@ export function pauseQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
     updatedAt: new Date()
   }
 
-  return { ok: true, action: "quest_pause", value: updatedQuest }
+  return { ok: true, action: "quest_mark_paused", value: updatedQuest }
 }
 
-export function abandonQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
+export function markQuestAbandoned(quest: Quest): ActionResult<Quest, ErrorCode> {
   if (quest.abandonedAt)
-    return { ok: false, action: "quest_abandon", error: allErrorCodes["QUEST_STATUS_ALREADY_ABANDONED"] }
+    return {
+      ok: false,
+      action: "quest_mark_abandoned",
+      error: allErrorCodes["QUEST_STATUS_ALREADY_ABANDONED"]
+    }
 
   const updatedQuest: Quest = {
     ...quest,
@@ -168,12 +176,16 @@ export function abandonQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
     updatedAt: new Date()
   }
 
-  return { ok: true, action: "quest_abandon", value: updatedQuest }
+  return { ok: true, action: "quest_mark_abandoned", value: updatedQuest }
 }
 
-export function completeQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
+export function markQuestCompleted(quest: Quest): ActionResult<Quest, ErrorCode> {
   if (quest.completedAt)
-    return { ok: false, action: "quest_complete", error: allErrorCodes["QUEST_STATUS_ALREADY_COMPLETED"] }
+    return {
+      ok: false,
+      action: "quest_mark_completed",
+      error: allErrorCodes["QUEST_STATUS_ALREADY_COMPLETED"]
+    }
 
   const updatedQuest: Quest = {
     ...quest,
@@ -188,12 +200,16 @@ export function completeQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
     updatedAt: new Date()
   }
 
-  return { ok: true, action: "quest_complete", value: updatedQuest }
+  return { ok: true, action: "quest_mark_completed", value: updatedQuest }
 }
 
-export function idleQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
+export function markQuestIdle(quest: Quest): ActionResult<Quest, ErrorCode> {
   if (quest.idledAt)
-    return { ok: false, action: "quest_idle", error: allErrorCodes["QUEST_STATUS_ALREADY_IDLED"] }
+    return {
+      ok: false,
+      action: "quest_mark_idle",
+      error: allErrorCodes["QUEST_STATUS_ALREADY_IDLED"]
+    }
 
   const updatedQuest: Quest = {
     ...quest,
@@ -208,12 +224,16 @@ export function idleQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
     updatedAt: new Date()
   }
 
-  return { ok: true, action: "quest_idle", value: updatedQuest }
+  return { ok: true, action: "quest_mark_idle", value: updatedQuest }
 }
 
-export function removeQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
+export function markQuestRemoved(quest: Quest): ActionResult<Quest, ErrorCode> {
   if (quest.removedAt)
-    return { ok: false, action: "quest_remove", error: allErrorCodes["QUEST_STATUS_ALREADY_REMOVED"] }
+    return {
+      ok: false,
+      action: "quest_mark_removed",
+      error: allErrorCodes["QUEST_STATUS_ALREADY_REMOVED"]
+    }
 
   const updatedQuest: Quest = {
     ...quest,
@@ -228,10 +248,10 @@ export function removeQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
     updatedAt: new Date()
   }
 
-  return { ok: true, action: "quest_remove", value: updatedQuest }
+  return { ok: true, action: "quest_mark_removed", value: updatedQuest }
 }
 
-export function restoreQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
+export function markQuestActive(quest: Quest): ActionResult<Quest, ErrorCode> {
   const updatedQuest: Quest = {
     ...quest,
     status: questLifecycleStatuses.ACTIVE,
@@ -240,7 +260,7 @@ export function restoreQuest(quest: Quest): ActionResult<Quest, ErrorCode> {
     updatedAt: new Date()
   }
 
-  return { ok: true, action: "quest_restore", value: updatedQuest }
+  return { ok: true, action: "quest_mark_active", value: updatedQuest }
 }
 
 export function createNote(questId: Quest["id"], text: Note["text"]): ActionResult<NoteDraft, ErrorCode> {
@@ -270,22 +290,22 @@ export function updateNote(note: Note, text: NoteDraft["text"]): ActionResult<No
   return { ok: true, action: "note_update", value: updatedNote }
 }
 
-export function removeNote(note: Note): ActionResult<Note, ErrorCode> {
+export function markNoteRemoved(note: Note): ActionResult<Note, ErrorCode> {
   const updatedNote: Note = {
     ...note,
     removedAt: new Date(),
     updatedAt: new Date()
   }
-  return { ok: true, action: "note_remove", value: updatedNote }
+  return { ok: true, action: "note_mark_removed", value: updatedNote }
 }
 
-export function restoreNote(note: Note): ActionResult<Note, ErrorCode> {
+export function markNoteActive(note: Note): ActionResult<Note, ErrorCode> {
   const updatedNote: Note = {
     ...note,
     removedAt: null,
     updatedAt: new Date()
   }
-  return { ok: true, action: "note_restore", value: updatedNote }
+  return { ok: true, action: "note_mark_active", value: updatedNote }
 }
 
 export function createProgress(
@@ -324,22 +344,22 @@ export function updateProgress(
   return { ok: true, action: "progress_update", value: updatedProgress }
 }
 
-export function removeProgress(progress: Progress): ActionResult<Progress, ErrorCode> {
+export function markProgressRemoved(progress: Progress): ActionResult<Progress, ErrorCode> {
   const updatedProgress: Progress = {
     ...progress,
     removedAt: new Date(),
     updatedAt: new Date()
   }
 
-  return { ok: true, action: "progress_remove", value: updatedProgress }
+  return { ok: true, action: "progress_mark_removed", value: updatedProgress }
 }
 
-export function restoreProgress(progress: Progress): ActionResult<Progress, ErrorCode> {
+export function markProgressActive(progress: Progress): ActionResult<Progress, ErrorCode> {
   const updatedProgress: Progress = {
     ...progress,
     removedAt: null,
     updatedAt: new Date()
   }
 
-  return { ok: true, action: "progress_restore", value: updatedProgress }
+  return { ok: true, action: "progress_mark_active", value: updatedProgress }
 }
